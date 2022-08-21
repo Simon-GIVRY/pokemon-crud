d14512416c8ac9ca266fa6e2734a65237f4a2213
Melodie Obszynski <melodieobszynski@hotmail.com>
2022-08-21 15:59:57 +0200

    pastrouverleserreurs

--- add.php
+++ add.php
@@ -10,9 +10,9 @@ if($connexion){
     $pkmn_generation = $_POST["generation"];
     $pkmn_release_date = $_POST["d-de-sortie"];
     $pkmn_support = $_POST["support-sortie"];
-
+    
     $execResult = $connexion->query("INSERT INTO pokemon_games (pkmn_game_name,pkmn_generation,pkmn_release_date,pkmn_support) VALUE ('$pkmn_game_name',$pkmn_generation,$pkmn_release_date,'$pkmn_support')");
-var_dump($pkmn_game_name);
+    var_dump($pkmn_game_name);
     // Parcours du tableau de résultats (afin d'afficher chaque utilisateur)
 //     
 }
