41c7d5f857dffd1b856e8249a87009ca4839caf8
Simon Givry <simongivr.sg@gmail.com>
2022-08-23 02:37:27 +0200

    erreur email fixé

--- inscription.php
+++ inscription.php
@@ -3,7 +3,7 @@
     require_once("../dbLog.php");
 
 
-    $username = $mail = $password = $confirmedPassword = $userPfp = "";
+   /*  $username = $mail = $password = $confirmedPassword = $userPfp = ""; */
     $errUserName = $errPassword = $errConfirmedPassword = $errMail = $errPfp = "";
 
     $aa ="";  /* var test to delete */
@@ -11,128 +11,131 @@
     if ($connexion) {
 
 
-          if (!empty($_POST)) {
+        /*   if (!empty($_POST)) {
             extract($_POST);
-        } 
+        }  */
         
         
         if (isset($_POST["inscription"])) {
 
-        foreach ($_POST as $fields => $values){
+            foreach ($_POST as $fields => $values){
     
-            if ($fields == "user_name" ){
-                
-                $username = trim($values);
-    
-                /* verification username */
-                if (empty($username)) {
-                    $errUserName = "Ce champ doit être rempli";
+                if ($fields == "user_name" ){
+                    
+                    $username = trim($values);
+        
+                    /* verification username */
+                    if (empty($username)) {
+                        $errUserName = "Ce champ doit être rempli";
+                    }
+
+                    else {
+                        if (!preg_match('/^[a-zA-Z0-9_]/', $username)) {
+                            $errUserName = "Le nom d'utilisateur peut seulement contenir des lettres, des nombres et des underscores";
+                        }
+                    }
+        
                 }
-
-                else {
-                    if (!preg_match('/^[a-zA-Z0-9_]/', $username)) {
-                        $errUserName = "Le nom d'utilisateur peut seulement contenir des lettres, des nombres et des underscores";
-                    }
-                }
-    
-            }
     
     
-            if ($fields === "user_password") {
-                
-                $password = trim($values);
+                if ($fields === "user_password") {
+                    
+                    $password = trim($values);
 
-                /* Password verification */
-                if (empty($password)) {
-                    $errPassword = "Ce champ doit être rempli.";
-                } else {
+                    /* Password verification */
+                    if (empty($password)) {
+                        $errPassword = "Ce champ doit être rempli.";
+                    } 
+                    else {
 
-                    /* Verify if password has more than 8 characters */
-                    if (strlen($password) <= 8) {
-                        $errPassword = "Votre mot de passe doit au moins faire 8 charactère de long.";
-                    }
+                        /* Verify if password has more than 8 characters */
+                        if (strlen($password) <= 8) {
+                            $errPassword = "Votre mot de passe doit au moins faire 8 charactère de long.";
+                        }
 
-                    /* Verify if password has at least one uppercase letter, one number and one  */
-                    if (!preg_match('/[A-Z]/', $password)  || !preg_match('/\d/', $password) || !preg_match("/[$&+,:;=?@#|'<>.-^*()%!]/", $password) ) {
-                        $errPassword = "Votre mot de passe doit contenir au moins une majuscule et au moins un chiffre.";
-                    }
+                        /* Verify if password has at least one uppercase letter, one number and one  */
+                        if (!preg_match('/[A-Z]/', $password)  || !preg_match('/\d/', $password) || !preg_match("/[$&+,:;=?@#|'<>.-^*()%!]/", $password) ) {
+                            $errPassword = "Votre mot de passe doit contenir au moins une majuscule et au moins un chiffre.";
+                        }
 
                     
+                    }
+                    /* Verify if there's any error so we can hash the password for the query */
+                    if (empty($errPassword)) {
+                        $hashedPassword=hash('sha512', htmlspecialchars($password) );
+                    }
                 }
-                /* Verify if there's any error so we can hash the password for the query */
-                if (empty($errPassword)) {
-                    $hashedPassword=hash('sha512', htmlspecialchars($password) );
-                }
-            }
     
-            if ($fields === "user_confirmedPassword") {
-               
-                $confirmedPassword = trim($values);
+                if ($fields === "user_confirmedPassword") {
+                
+                    $confirmedPassword = trim($values);
 
-                /* confirmed password verification */
-                if (empty($confirmedPassword)) {
-                    $errConfirmedPassword = "Ce champ doit être rempli.";
-                } elseif ($password != $confirmedPassword) {
+                    /* confirmed password verification */
+                    if (empty($confirmedPassword)) {
+                        $errConfirmedPassword = "Ce champ doit être rempli.";
+                    } 
+                    elseif ($password != $confirmedPassword) {
                         $errConfirmedPassword = "Vos mots de passes ne correspondent pas.";
                     }
                 }
-            }
-    
-            if ($fields === "user_email") {
-               
-                $mail = trim($values);
+            
 
-                /* verification mail */
-                if (empty($mail)) {
-                    $errMail = "Ce champ doit être rempli";
-                } else {
-
-                    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-                        $errMail = "Votre adresse Email n'est pas conforme.";
-                    }
-                }
-            }
+            
+            
     
-            if ($fields === "user_img"){
-    
-                $userPfp = trim($values);
-    
-                /* verification pfp */
-                if (!empty($userPfp)) {
+                if ($fields === "user_mail") {
+                    $mail = trim($values);
                     
-                    if (!filter_var($userPfp, FILTER_VALIDATE_URL)) {
-                        $errPfp = "Votre url n'est pas conforme.";
+                    /* verification mail */ 
+                    if (empty($mail)) {
+                        $errMail = "Ce champ doit être rempli";
+                    } 
+                    elseif (!filter_var($mail, FILTER_VALIDATE_EMAIL))  {
+                        $errMail = "Votre adresse email n'est pas conforme.";
+                        
                     }
                 }
+            
+
+            
+
+    
+                if ($fields === "user_img"){
+        
+                    $userPfp = trim($values);
+        
+                    /* verification pfp */
+                    if (!empty($userPfp)) {
+                        
+                        if (!filter_var($userPfp, FILTER_VALIDATE_URL)) {
+                            $errPfp = "Votre url n'est pas conforme.";
+                        }
+                    }
+                }
+                /* var_dump($username); */
+                /* var_dump($_POST); */
+                /* var_dump($fields); */
+                /* var_dump($values); */
+                /* var_dump($username);
+                var_dump($password);
+                var_dump($mail); */
             }
-            /* var_dump($errUserName);
-            var_dump($errMail);
-            var_dump($errPassword);
-            var_dump($errConfirmedPassword); */
-             /* var_dump($_POST); */
-            var_dump($fields);
-            /* var_dump($values); */
-            var_dump($username);
-            var_dump($password);
-            var_dump($mail);
+            
+            
+            /* ENVOIE REQUETE */
+            if (empty($errUserName) && empty($errMail) && empty($errPassword) && empty($errConfirmedPassword) && empty($errPfp)) {
+            /*   $connexion->query("INSERT INTO users (user_name, user_email, user_password, user_img) VALUES ('$username', '$mail', '$hashedPassword', '$userPfp')"); */
+                $aa = "test réussi";
+                /* header("Location: ./inscriptionReussi.php"); */
+            }
         }
 
-
-
-        /* ENVOIE REQUETE */
-        if (empty($errUserName) && empty($errMail) && empty($errPassword) && empty($errConfirmedPassword) && empty($errPfp)) {
-          /*   $connexion->query("INSERT INTO users (user_name, user_email, user_password, user_img) VALUES ('$username', '$mail', '$hashedPassword', '$userPfp')"); */
-            $aa = "test réussi";
-
-            /* header("Location: ./inscriptionReussi.php"); */
-
-
-        }
+        
         
     } 
 
-
-
+    
+    
 ?>
 
 
@@ -160,7 +163,7 @@
 
         <label for="email">Entrez votre adresse mail&nbsp;:</label>
         <div> <?php echo $errMail ?></div>
-        <input type="email" name="user_email" value="<?php if (isset($mail)) { echo $mail; } ?>" id="email">
+        <input type="email" name="user_mail" value="<?php if (isset($mail)) { echo $mail; } ?>"  id="email">
         <br>
 
         <label for="password">Entrez votre mot de passe&nbsp;:</label>
